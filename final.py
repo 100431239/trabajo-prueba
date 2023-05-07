545a44dd237ad9348ddd2058c6e35c321e50d0ca
100431239 <132850869+100431239@users.noreply.github.com>
2023-05-08 01:07:51 +0200

    Update final.py

--- final.py
+++ final.py
@@ -53,10 +53,6 @@ st.markdown('<h2 style="color:#1c7edc;">Evolution of received grants according t
 activity_type_grants_by_year = participants_df.groupby(["year", "activityType"])["Total_Grants_Received"].sum().unstack().fillna(0)
 st.bar_chart(activity_type_grants_by_year)
 
-# Extra: Display a pie chart with distribution of activity types among partners in the selected country
-st.markdown('<h2 style="color:#1c7edc;">Distribution of activity types among partners</h2>', unsafe_allow_html=True)
-activity_type_counts = participants_df["activityType"].value_counts()
-st.area_chart(activity_type_counts)
 
 # Extra: Displaying list/stats of projects according to the project keywords
 keywords = st.multiselect("Select project keywords to filter:", options=['AI', 'IoT', 'Big Data', 'Cloud', 'Security'])
