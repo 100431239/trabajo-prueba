ed2b9c26301da71b0d6a8cee3098dfdcf5d16fda
100431239 <132850869+100431239@users.noreply.github.com>
2023-05-07 23:32:40 +0200

    Update final.py

--- final.py
+++ final.py
@@ -22,6 +22,7 @@ acros = list(countries_df["Acronym"])
 
 # FR2.6: Ask the user to input a country acronym
 selected_country_acronym = st.selectbox("Please select a country acronym from the dropdown menu:", acros)
+country_name = countries_df.loc[countries_df["Acronym"] == selected_country_acronym, "Country"].values[0]
 
 # FR2.8: Generate a new dataframe of participants with the total amount of received grants per partner in the
 # selected country
@@ -31,7 +32,7 @@ participants_df = pd.read_sql_query(
     f"organizationURL",  conn)
 
 # FR2.9: Display the generated dataset, in descending order by received grants
-st.subheader("Participants in descending order by received grants")
+st.subheader(f"Participants in {country_name}")
 st.write(participants_df.sort_values("Total_Grants_Received", ascending=False))
 
 # FR2.10: Generate a new project dataframe with the project coordinators from the selected country
@@ -41,7 +42,7 @@ project_coordinators_df = pd.read_sql_query(
     f"GROUP BY shortName, name, activityType, projectAcronym", conn)
 
 # FR2.11: Display the generated coordinator dataset, in ascending order by shortName
-st.subheader("Project coordinators in ascending order by shortName")
+st.subheader(f"Project coordinators in {country_name}")
 st.write(project_coordinators_df.sort_values("shortName", ascending=True))
 
 # FR2.12: Save the generated datasets (participants, and project coordinators) in a CSV file
